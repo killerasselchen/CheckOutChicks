5586d5922696d101a8df44ded184a27d02177beb
TimoF <timo.fabricius@gmx.de>
2015-07-26 22:19:56 +0200

    StickyPuddle instantiate at Player, but it doesnt wok corectly and spawn with own center on Player who use it.

--- CheckOutChicks/Assets/Scripts/Power_Up_Management/Power_Ups/Sticky_Puddle.cs
+++ CheckOutChicks/Assets/Scripts/Power_Up_Management/Power_Ups/Sticky_Puddle.cs
@@ -6,7 +6,7 @@ class Sticky_Puddle : Power_Up
     public override void Use(Player player)
     {
         GameObject Puddle = GameObject.Instantiate(player.sticky_Puddle_Prefab) as GameObject;
-        Puddle.transform.localPosition = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z -2);
+        Puddle.transform.localPosition = new Vector3(player.transform.localPosition.x, Puddle.transform.position.y, player.transform.localPosition.z);
         Puddle.transform.localRotation = player.transform.localRotation;
     }
     
