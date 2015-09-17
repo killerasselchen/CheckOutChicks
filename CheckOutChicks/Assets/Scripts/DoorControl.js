c790ad5c5a59c2449669536b71dc4917dc0b2c92
Claudi <mr.bigglesworth@web.de>
2015-09-17 15:48:36 +0200

    added: Tueranimationen, powerups

---
+++ CheckOutChicks/Assets/Scripts/DoorControl.js
@@ -0,0 +1,54 @@
+﻿#pragma strict
+//Build by Christoph
+//Adjusted by Sharon
+
+var doorObject : GameObject;
+private var doorAnimator : Animator;
+
+function Start () {
+	doorAnimator = doorObject.GetComponent(Animator);
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter( other : Collider)
+{
+	if(other.gameObject.tag == "Player_One")
+	{
+		doorAnimator.SetBool("infrontOfDoor", true);
+	}
+	if(other.gameObject.tag == "Player_Two")
+	{
+		doorAnimator.SetBool("infrontOfDoor", true);
+	}
+	if(other.gameObject.tag == "Player_Three")
+	{
+		doorAnimator.SetBool("infrontOfDoor", true);
+	}
+	if(other.gameObject.tag == "Player_Four")
+	{
+		doorAnimator.SetBool("infrontOfDoor", true);
+	}
+}
+
+function OnTriggerExit( other : Collider)
+{
+	if(other.gameObject.tag == "Player_One")
+	{
+		doorAnimator.SetBool("infrontOfDoor", false);
+	}
+	if(other.gameObject.tag == "Player_Two")
+	{
+		doorAnimator.SetBool("infrontOfDoor", false);
+	}
+	if(other.gameObject.tag == "Player_Three")
+	{
+		doorAnimator.SetBool("infrontOfDoor", false);
+	}
+	if(other.gameObject.tag == "Player_Four")
+	{
+		doorAnimator.SetBool("infrontOfDoor", false);
+	}
+}
